42545f2c3e344b68bca8d2cd04dc23988cdc7c21
Yevgen9 <110126038+Yevgen9@users.noreply.github.com>
2024-05-01 16:09:58 +0000

    Deploying to gh-pages from @ Yevgen9/goit-react-hw-05-movies@d3c6c99e860b0942b33768d0df6d58d954f4d531 🚀

---
+++ static/js/29.d6f0c3c2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[29],{29:function(e,t,i){i.r(t);i(791);var o=i(87),n=i(184);t.default=function(){return(0,n.jsxs)("div",{children:[(0,n.jsx)("h1",{children:"Page Not Found"}),(0,n.jsx)("p",{children:"Sorry, but the page you were trying to view does not exist."}),(0,n.jsx)("p",{children:"Go to the main page!"}),(0,n.jsx)(o.OL,{to:"/",children:"Home"})]})}}}]);
+//# sourceMappingURL=29.d6f0c3c2.chunk.js.map
